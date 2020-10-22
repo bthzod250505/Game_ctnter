8c7cccd4f678744c30bee07acbd6a6d324ceb70d
Behzod250505 <73109143+bthzod250505@users.noreply.github.com>
2020-10-22 12:37:56 +0500

    Add files via upload

---
+++ web_css/script.js
@@ -0,0 +1,14 @@
+
+ $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+$('.pa').click(function(){
+			$('.fg').toggleClass('active')
+		})
